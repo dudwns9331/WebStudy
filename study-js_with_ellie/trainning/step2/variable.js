182144529d32b2dfdcc9c6d997e898cfb7a3cdbb
dudwns9331 <dudwns1045@naver.com>
2021-02-06 20:50:47 +0900

    Update step2 (#2)

--- study-js_with_ellie/trainning/step2/variable.js
+++ study-js_with_ellie/trainning/step2/variable.js
@@ -1,8 +1,7 @@
 // 순수 자바스크립트를 사용하는데에는 단어를 사용하는게 좋다.
 // added ECMAScript 5
 // 상식적인 범주에서 사용이 가능, 성능도 개선된다.
-'use strict';
-
+"use strict";
 
 // 2. variable
 // let (added in ES6) let은 ES6에서 추가되었다.
@@ -11,28 +10,26 @@
 // var hoisting이라고 한다. 어디에 선언했냐에 상관없이 제일 위로 끌어 올려준다.
 // block scope이 없다.
 
-let globalName = 'global name';
+let globalName = "global name";
 
-{ 
-    let name = 'yeongjun';
-    console.log(name);
-    name = 'hello';
-    console.log(name);
-        
+{
+  let name = "yeongjun";
+  console.log(name);
+  name = "hello";
+  console.log(name);
 }
 
 console.log(name);
 console.log(globalName);
 
 {
-    age = 4;
-    var age;
+  age = 4;
+  var age;
 }
 
 console.log(age);
 
-
-// 3. constns
+// 3. const
 // 상수 변경이 불가능
 // favor immutable data type always for a few reasons:
 //  - security
@@ -41,7 +38,6 @@ console.log(age);
 
 const daysInWeek = 7;
 const maxNumber = 5;
-
 
 // 4. Variable types
 // primitive, sigle item: number, string, boolean, null, undefind, symbol
@@ -59,22 +55,20 @@ console.log(`value: ${size}, type: ${typeof size}`);
 
 const infinity = 1 / 0;
 const negativeInfinity = -1 / 0;
-const nAn = 'not a number' / 2;
+const nAn = "not a number" / 2;
 
 console.log(infinity);
 console.log(negativeInfinity);
 console.log(nAn);
 
-
 // string
-const char = 'c';
-const brendan = 'brendan';
-const greeting = 'hello ' + brendan;
+const char = "c";
+const brendan = "brendan";
+const greeting = "hello " + brendan;
 console.log(`value : ${greeting}, type : ${typeof greeting}`);
 const helloBob = `hi ${brendan}!`; // template literals (string) 백틱으로 묶고 ${}를 이용한다.
 console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
 // console.log(`value: ` + helloBob + ' type: ' + typeof helloBob)
-
 
 // boolean
 // false: 0, null, undefined, NaN, ''
@@ -94,24 +88,23 @@ console.log(`value: ${x}, type: ${typeof x}`);
 
 // symbol, create unique identifiers for object
 // 두 개의 심볼은 다르다.
-const symbol1 = Symbol('id');
-const symbol2 = Symbol('id');
+const symbol1 = Symbol("id");
+const symbol2 = Symbol("id");
 console.log(symbol1 === symbol2);
-const gSymbol1 = Symbol.for('id');
-const gSymbol2 = Symbol.for('id');
-console.log(gSymbol1 === gSymbol2);         // true
+const gSymbol1 = Symbol.for("id");
+const gSymbol2 = Symbol.for("id");
+console.log(gSymbol1 === gSymbol2); // true
 console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
-
 
 // 5. Dynamic typing : dynamically typed language
 
-let text = 'hello';
+let text = "hello";
 console.log(text.charAt(0));
 console.log(`value : ${text}, type: ${typeof text}`);
 text = 1;
 console.log(`value : ${text}, type: ${typeof text}`);
-text = '7' + 5;
+text = "7" + 5;
 console.log(`value : ${text}, type: ${typeof text}`);
-text = '8' / '2';
+text = "8" / "2";
 console.log(`value : ${text}, type: ${typeof text}`);
 console.log(text.charAt(0));
