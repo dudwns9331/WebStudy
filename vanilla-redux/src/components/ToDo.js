33f74c24e361587f6e3856813b36c39f20dfd24f
dudwns9331 <dudwns1045@naver.com>
2021-05-02 16:19:19 +0900

    Use Redux-ToolKit, Example : ToDoList (#5) Done :)

--- vanilla-redux/src/components/ToDo.js
+++ vanilla-redux/src/components/ToDo.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { actionCreators } from "../store";
+import { remove } from "../store";
 
 function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>X</button>
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>X</button>
     </li>
   );
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
   return {
-    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+    onBtnClick: () => dispatch(remove(ownProps.id)),
   };
 }
 export default connect(null, mapDispatchToProps)(ToDo);
--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,41 +1,52 @@
 // store.js
 // ToDo List의 State를 저장하고 수정하는 곳
 
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import {
+  configureStore,
+  // createAction,
+  // createReducer,
+  createSlice,
+} from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+// https://redux-toolkit.js.org/api/createReducer 리덕스 툴킷 CreateReducer 예시 및 설명
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+// const reducer = createReducer([], {
+//   // push는 mutate 하게 해준다. 상태를 return 하지 않는다. 변경만 시킨다는 뜻
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
+//   // 혹은 새로운 state를 return 해야 한다.
+//   [deleteToDo]: (state, action) => {
+//     state.filter((toDo) => toDo.id !== action.payload);
+//   },
+// });
 
-const store = createStore(reducer);
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
+  },
+});
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
-
-export default store;
+export const { add, remove } = toDos.actions;
+export default configureStore({ reducer: toDos.reducer });
